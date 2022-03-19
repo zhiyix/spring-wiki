39ca9d6ba4d9a9ac0791cb0177139cf6b7f0baed
yunzhiwu <imetre@163.com>
2022-03-19 19:46:14 +0800

    0.3.5 集成mybatis generator生成ebook表代码

---
+++ src/main/java/com/imetre/wiki/mapper/EBookMapper.java
@@ -0,0 +1,30 @@
+package com.imetre.wiki.mapper;
+
+import com.imetre.wiki.domain.EBook;
+import com.imetre.wiki.domain.EBookExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface EBookMapper {
+    long countByExample(EBookExample example);
+
+    int deleteByExample(EBookExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(EBook record);
+
+    int insertSelective(EBook record);
+
+    List<EBook> selectByExample(EBookExample example);
+
+    EBook selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") EBook record, @Param("example") EBookExample example);
+
+    int updateByExample(@Param("record") EBook record, @Param("example") EBookExample example);
+
+    int updateByPrimaryKeySelective(EBook record);
+
+    int updateByPrimaryKey(EBook record);
+}
