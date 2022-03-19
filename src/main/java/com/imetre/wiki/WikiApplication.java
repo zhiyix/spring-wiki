c14c48af7adcfa97b433595a613a32fa6530057c
yunzhiwu <imetre@163.com>
2022-03-19 13:32:20 +0800

    0.2.5 增加logback配置文件

--- src/main/java/com/imetre/wiki/WikiApplication.java
+++ src/main/java/com/imetre/wiki/WikiApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WikiApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(WikiApplication.class, args);
     }
-
 }
