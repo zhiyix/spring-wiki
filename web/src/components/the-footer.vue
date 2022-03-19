b8208f6109b79bcf40066575bf8bd4046055c80e
yunzhiwu <imetre@163.com>
2022-03-19 22:16:48 +0800

    0.4.7 制作the-header和the-footer组件

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,34 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    Ant Design ©2018 Created by Ant UED
+  </a-layout-footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'TheFooter',
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ web/src/components/the-header.vue
@@ -0,0 +1,49 @@
+<template>
+  <a-layout-header class="header">
+    <div class="logo" />
+    <a-menu
+        theme="dark"
+        mode="horizontal"
+        v-model:selectedKeys="selectedKeys"
+        :style="{ lineHeight: '64px' }"
+    >
+      <a-menu-item key="1">nav 1</a-menu-item>
+      <a-menu-item key="2">nav 2</a-menu-item>
+      <a-menu-item key="3">nav 3</a-menu-item>
+    </a-menu>
+  </a-layout-header>
+</template>
+
+<script lang="ts">
+import {defineComponent, ref} from 'vue';
+
+export default defineComponent({
+  name: 'TheHeader',
+  props: {
+    msg: String,
+  },
+  setup() {
+    return {
+      selectedKeys: ref<string[]>(['2']),
+    };
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
