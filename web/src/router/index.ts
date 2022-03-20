d2ab9cebe7145481c199d9cb9990cff0baebceb0
yunzhiwu <imetre@163.com>
2022-03-20 19:48:14 +0800

    0.6.2 增加电子书管理页面

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/home.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -13,7 +13,12 @@ const routes: Array<RouteRecordRaw> = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/about.vue')
+  },
+  {
+    path: '/admin',
+    name: 'Admin',
+    component: () => import(/* webpackChunkName: "about" */ '../views/admin/admin-ebook.vue')
   }
 ]
 
